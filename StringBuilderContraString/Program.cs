622c5cdce1e719532672f90c7bf6a2e8aabf2261
Milton <josemiltondev@gmail.com>
2021-03-09 18:30:51 -0300

    Add project files.

---
+++ StringBuilderContraString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StringBuilderContraString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int loops = 100_000;
+            DateTime inicio, fim;
+            int i;
+
+            string stringFinal = "";
+            inicio = DateTime.Now;
+            for (i = 0; i < loops; i++)
+            {
+                stringFinal += i.ToString() + " ";
+            }
+
+            fim = DateTime.Now;
+            //Console.WriteLine(stringFinal);
+            Console.WriteLine("Concatenação de string levou:" + (fim - inicio).TotalSeconds + " segundos.");
+
+
+            //inicio = DateTime.Now;
+            //System.Text.StringBuilder sb = new System.Text.StringBuilder("");
+            //for (i = 0; i < loops; i++)
+            //{
+            //    sb.Append(i.ToString() + " ");
+            //}
+            //fim = DateTime.Now;
+
+            //Console.WriteLine(sb.ToString());
+            //Console.WriteLine("Concatenação com o StringBuilder levou: " + (fim - inicio).TotalSeconds + " segundos.");
+
+        }
+    }
+}
